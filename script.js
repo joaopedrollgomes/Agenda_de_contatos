0195ad655485dcfdcad9f4bc906c348b83bdab5d
joaopedrollgomes <jpedrollgomes@gmail.com>
2023-10-13 12:59:04 -0300

    code update

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
-// Lista de contatos do localStorage
-let listaContatos = JSON.parse(localStorage.getItem('contatos')) || [];
-
-//Local na página index onde será add os contatos 
-let listaNovoContato = document.getElementById('listaContatos');
-
-// Loop para add os contatos ao index
-for (let i = 0; i < listaContatos.length; i++) {
-    let contato = listaContatos[i];
-    let novoContato = document.createElement('li');
-    novoContato.innerHTML = `
-        <p>${contato.nome}</p>
-        <p>${contato.numero}</p>
-        <p>${contato.email}</p>
-    `;
-    listaNovoContato.appendChild(novoContato);
+class Contato {
+    constructor(nome, numero, email) {
+        this.nome = nome;
+        this.numero = numero;
+        this.email = email;
+    }
 }
 
-localStorage.clear();
+const contatos = [];
+
+function addContato(nome, numero, email) {
+    const contato = new Contato(nome, numero, email);
+    contatos.push(contato);
+    listaAtualizada();
+}
+
+// Função para atualizar a lista de contatos
+function listaAtualizada() {
+    const listaContatos = document.getElementById('listaContatos');
+    listaContatos.innerHTML = '';
+
+    contatos.forEach((contato, index) => {
+        const itemLista = document.createElement('li');
+        itemLista.className = 'style-contato';
+
+        itemLista.innerHTML = `
+        <span class = "nome">${contato.nome}</span> <span class = "numero">${contato.numero}</span> <span class = "email">${contato.email}</span>
+        <button class="editar-button" onclick="editarContato(${index})">Editar</button>
+        <button class="deletar-button" onclick="deletarContato(${index})">Excluir</button>
+      `;
+
+        listaContatos.appendChild(itemLista);
+    });
+}
+
+// Função para editar um contato
+function editarContato(index) {
+    const novoNome = prompt('Novo Nome:', contatos[index].nome);
+    const novoNumero = prompt('Novo Número:', contatos[index].numero);
+    const novoEmail = prompt('Novo Email:', contatos[index].email);
+
+    if (novoNome !== null && novoNumero !== null && novoEmail !== null) {
+        contatos[index] = new Contato(novoNome, novoNumero, novoEmail);
+        listaAtualizada();
+    }
+}
+
+// Função para excluir um contato
+function deletarContato(index) {
+    if (confirm('Tem certeza de que deseja excluir este contato?')) {
+        contatos.splice(index, 1); // Remove o contato
+        listaAtualizada();
+    }
+}
+
+// "Ativando" o formulário de adição de contato
+const formulario = document.querySelector('form');
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const nome = document.querySelector('input[name="nome"]').value;
+    const numero = document.querySelector('input[name="telefone"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    addContato(nome, numero, email);
+    formulario.reset();
+});
+
+listaAtualizada();
